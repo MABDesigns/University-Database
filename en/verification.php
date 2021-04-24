25a0c6f0ca38c0c24d7d40dfb88991b72beff2c2
Mostepha Amine Bdeira <49349658+MABDesigns@users.noreply.github.com>
2021-04-24 02:51:53 +0100

    Starting On Github

---
+++ en/verification.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+if(isset($_POST['username']) && isset($_POST['password']))
+{
+    // connexion à la base de données
+    $db_username = 'epiz_26539772';
+    $db_password = 'OqApOz7p2z1we';
+    $db_name     = 'epiz_26539772_university';
+    $db_host     = 'sql101.epizy.com';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die('could not connect to database');
+    
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+    
+    if($username !== "" && $password !== "")
+    {
+        $requete = "SELECT count(*) FROM loginform where 
+              User = '".$username."' and Pass = '".$password."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['username'] = $username;
+           header('Location: https://google.dz/');
+        }
+        else
+        {
+           header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: login.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
